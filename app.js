faf8c7796e24e90129185773825bcfd4d2ce0505
abdul qadir <154596304+Abdul-Qadir-Gull@users.noreply.github.com>
2024-02-28 16:30:16 +0500

    assignment2 of JS (chapter 2 and  chapter 3)

---
+++ app.js
@@ -0,0 +1,110 @@
+//**********************// CHAPTER 2 ASSIGNMENNT ANSWERS****************************
+
+
+// Q1. Declare a variable called username.
+// ANS1. var username ;
+// console.log(username);
+
+
+// Q2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// ANS2. var myName = "Abdul Qadir Gull";
+// console.log(myName)
+
+
+// Q3. 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// ANS3. var message = "Hello World"
+// alert(message)
+
+
+// Q4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// ANS4.var studentname = "John Doe";
+// var studentage = 15;
+// var studentskills = "Certified Mobile Application Development";
+// -----------------
+// alert(studentname)
+// alert(studentage)
+// alert(studentskills)
+
+
+// Q5. Write a script to display the following alert using one JS
+// variable:
+// ANS5.var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza)
+
+
+// Q6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+// ANS6 . ANvar email = "My Email Adress Is abdulqadirgull0@gmail.com"
+// alert(email)
+
+
+// Q7.Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// ANS7. var book = "I am trying to learn from the book A smarter\n way to learn JavaScript"
+// alert(book)
+
+
+// Q8. Write a script to display this in browser through JS
+// ANS8. document.write("<b>YAH!! I can write HTML content through javascript</b>");
+
+
+// Q9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// ANS9.var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+
+
+
+
+
+
+
+
+//**********************// CHAPTER 3 ASSIGNMENNT ANSWERS****************************
+
+// Q1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// ANS1 var age = 18;
+// alert(age)
+
+
+// Q2.  Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// ANS2.var trackingvisitors = "you have visited 14 times today on this website"
+// alert(trackingvisitors)
+
+
+// Q3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// ANS3. var birthyear =   2005;
+// document.write("My birth year is",(birthyear),"\n data type of my declared variable is number")
+
+
+
+
+// Q4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// ANS4.var Visitorsname = "John Doe"
+// var productTitle = "TShirt(s)"
+// var quantity = 5;
+// document.write(Visitorsname,"<span> <b>ordered</b> </span>", quantity, productTitle,"<span> <b>On XYZ clothing store</b> </span>")
