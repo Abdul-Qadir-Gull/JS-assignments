b195326fd44e9e8ccb0a192c85ee6ede017a9e13
abdul qadir <154596304+Abdul-Qadir-Gull@users.noreply.github.com>
2024-02-28 15:06:08 +0500

    assignment 1 of js

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+// Assignment number 1
+
+
+//  Q1 1. Write a script to greet your website visitor using JS alert
+// box. :::::::::::
+// alert("Hello! welcome ro my site");
+// Swal.fire("Hello! welcome ro my site");
+// :::::::::::::::
+
+// Q2. Write a script to display following message on your web
+// page:::::::
+// alert("ERROR! please enter a valid password")
+// ::::::::::::::::::
+
+// Q3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// alert("Welcome to JS land...\n Happy Coding!!")
+
+// Q4. Write a script to display following messages in sequence:
+// alert("Welcome to JS land...")
+// alert("happy coding ch")
+
+// alert("Happy coding!\n  prevent this page from creating additional change"  )
+
+// Q5. Generate the following message through browser’s
+// developer console:
+// console.log("HELLO!! I can run my javascript through web browser");
+
+// Q6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// ALREADY DONE IN PREVIOUS ASSIGNMENT
